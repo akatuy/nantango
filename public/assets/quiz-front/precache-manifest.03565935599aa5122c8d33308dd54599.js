ae6fcf43f0c9b0e653cb8544fb578712c966b9a4
t-cool <hiroki802@hotmail.com>
2020-01-27 17:19:36 +0900

    trying to fix the path to resource from utopian server

---
+++ public/assets/quiz-front/precache-manifest.03565935599aa5122c8d33308dd54599.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933e9daf9bf191ba3202d6433ac99c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3475cc6ca7b647e3c380",
+    "url": "./static/css/2.e7f7683d.chunk.css"
+  },
+  {
+    "revision": "5fd5fadf17096220edb6",
+    "url": "./static/css/main.233101f6.chunk.css"
+  },
+  {
+    "revision": "3475cc6ca7b647e3c380",
+    "url": "./static/js/2.63785182.chunk.js"
+  },
+  {
+    "revision": "3b56f65725ff556e3982377891ad07f6",
+    "url": "./static/js/2.63785182.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fd5fadf17096220edb6",
+    "url": "./static/js/main.dd016aa4.chunk.js"
+  },
+  {
+    "revision": "e160a5f08b1cb770019b",
+    "url": "./static/js/runtime-main.7b7700e6.js"
+  }
+]);
